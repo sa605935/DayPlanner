8a2640d03c3fe0bfd734e956023c73b91e781ec0
Shannon Alexander <shannonmariealexander94@gmail.com>
2021-01-27 20:05:53 -0500

    Added styling for 'clear planner' button, container, task description space and hour row

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var currentHour = moment().hours();
-var hoursArray = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var hoursArray = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 $('#currentDay').text(moment().format('dddd, MMMM Do YYYY, h:mm a'));
 
